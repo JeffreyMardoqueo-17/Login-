cc6c47bef7cd08fe775866d1d21cb767d4eeaf48
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2023-05-02 10:01:11 -0600

    Jeffrey Mardoqueo

---
+++ JS/index.js
@@ -0,0 +1,7 @@
+//con esto logramos el efecto de entrada
+window.sr = ScrollReveal();
+    sr.reveal('.form-contenido', {
+        duration: 900,
+        origin: 'left',
+        distance: '-110px'
+    });
